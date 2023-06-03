de8a5fed0656e8205c605676c77f47a5c15b7f44
rythm01 <ridhamchauhan693@gmail.com>
2023-06-03 19:04:57 +0530

    added megaDropDown list for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Footer from './components/Footer'
 import Leandingpage from './components/Leandingpage'
 import NavbarCom from './components/Navbar'
+import NavbarMenu from './components/navbarMenu'
 
 function App() {
 
   return (
     <>
       <NavbarCom />
+      <NavbarMenu />
       <Leandingpage></Leandingpage>
       <Footer></Footer>
     </>

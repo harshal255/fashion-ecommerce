79777c34bf7639d94c64d97a62660235002fd1bf
Harshal Kahar <200280107119@ldceahm.gujgov.edu.in>
2023-06-11 13:54:01 +0530

    Collection page and Not found page created

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
+import { BrowserRouter } from 'react-router-dom';
 import "@fontsource/open-sans"; // Defaults to weight 400
 import "@fontsource/open-sans/400.css"; // Specify weight
 import "@fontsource/open-sans/400-italic.css"; // Specify weight and style
@@ -11,7 +12,9 @@ import "@fontsource/open-sans/400-italic.css"; // Specify weight and style
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
+    <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 )

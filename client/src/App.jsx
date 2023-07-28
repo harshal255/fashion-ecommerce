96d4b38c92c01a417a5d3014d5e31330a28b3c5d
Harshal Kahar <93152436+harshal255@users.noreply.github.com>
2023-07-28 10:54:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,253 @@
-
+import { useContext, useEffect, useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
+import NavbarCom from './components/Navbar'
+import NavbarMenu from './components/navbarMenu'
 import MobileFooter from './components/MobileFooter'
-import Agents from './pages/Agents'
-import Becomeagent from './pages/Becomeagent'
-import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
-import Property from './pages/Property'
-
-
+import NoMatch from './components/NoMatch'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgetPassword from './pages/ForgetPassword'
+import CollectionDetails from './pages/collectionDetails'
+import CheckoutForm from './pages/CheckoutForm'
+import AdminPanel from './pages/Admin/AdminPanel'
+import CreateProduct from './pages/Admin/createProduct'
+import AuthContext, { AuthProvider } from './AuthContext';
+import Orders from './pages/Orders/orders'
+import UserDetails from './components/userdetail'
+import TopCollections from './pages/TopCollections'
+import GownCollections from './pages/GownCollections'
+import SareesCollections from './pages/SareesCollections'
+import LehengaCholiCollections from './pages/LehengaCholiCollections'
+import UpdateProduct from './pages/Admin/UpdateProduct'
+import { CartProvider } from './CartContext'
 
 function App() {
-
-
   return (
-    <div className='mx-0 px-0 max-h-fit overflow-y-clip'>
-      <Header></Header>
+    <AuthProvider>
+      <CartProvider>
+        <AppRoutes />
+      </CartProvider>
+    </AuthProvider>
+  );
+}
+
+function AppRoutes() {
+  const { isLoggedIN, fetchUserProfile } = useContext(AuthContext);
+  const [userDetails, setUserDetails] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isLoggedIN) {
+        try {
+          const userProfile = await fetchUserProfile();
+          setUserDetails(userProfile);
+        } catch (error) {
+          console.error('Error fetching user details:', error);
+        }
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const isAdmin = userDetails?.role === 'admin';
+  return (
+    <>
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <Home></Home>
-              <MobileFooter></MobileFooter>
-              <Footer></Footer>
+              <NavbarCom />
+              <NavbarMenu />
+              <Home />
+              <MobileFooter />
+              <Footer />
             </>
           }
         ></Route>
         <Route
-          path="/becomeagent"
+          path="/LehengaCholiCollections"
           element={
             <>
-              <Becomeagent></Becomeagent>
-              <MobileFooter></MobileFooter>
-              <Footer></Footer>
+              <NavbarCom />
+              <NavbarMenu />
+              <LehengaCholiCollections />
+              <MobileFooter />
+              <Footer />
             </>
           }
         ></Route>
         <Route
-          path="/agents"
+          path="/SareesCollections"
           element={
             <>
-              <Agents></Agents>
-              <MobileFooter></MobileFooter>
-              <Footer></Footer>
+              <NavbarCom />
+              <NavbarMenu />
+              <SareesCollections />
+              <MobileFooter />
+              <Footer />
             </>
           }
         ></Route>
         <Route
-          path="/property"
+          path="/GownCollections"
           element={
             <>
-              <Property></Property>
-              <MobileFooter></MobileFooter>
-              <Footer></Footer>
+              <NavbarCom />
+              <NavbarMenu />
+              <GownCollections />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/TopCollections"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <TopCollections />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/user"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <UserDetails />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/collections/details"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <CollectionDetails />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        {isAdmin && (
+          <Route
+            path="/TopCollections"
+            element={
+              <>
+                <NavbarCom />
+                <AdminPanel />
+                <MobileFooter />
+                <Footer />
+              </>
+            }
+          ></Route>
+        )}
+        {isAdmin && (
+          <Route
+            path="/collections/details"
+            element={
+              <>
+                <NavbarCom />
+                <CreateProduct />
+                <MobileFooter />
+                <Footer />
+              </>
+            }
+          ></Route>
+        )}
+        {isAdmin && (
+          <Route
+            path="/admin/Updateproduct"
+            element={
+              <>
+                <NavbarCom />
+                <UpdateProduct />
+                <MobileFooter />
+                <Footer />
+              </>
+            }
+          ></Route>
+        )}
+        <Route
+          path="/login"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <Login />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/register"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <Register />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/recover"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <ForgetPassword />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/orders"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <Orders />
+              <MobileFooter />
+              <Footer />
             </>
           }
         ></Route>
 
-      </Routes>
-
-
-    </div>
-  )
+        <Route
+          path="/checkout"
+          element={<CheckoutForm />}
+        ></Route>
+        <Route
+          path="*"
+          element={
+            <>
+              <NavbarCom />
+              <NavbarMenu />
+              <NoMatch />
+              <MobileFooter />
+              <Footer />
+            </>
+          }
+        ></Route>
+      </Routes >
+    </>
+  );
 }
 
-export default App
-
+export default App;
